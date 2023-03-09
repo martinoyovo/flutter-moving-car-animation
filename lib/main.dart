325cef55d1af574e8fc06eb12d0c77e794a5f6c5
Martino Yovo <martinosoyen@gmail.com>
2023-03-09 10:41:21 +0000

    Car animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_logo_animation/custom_loader.dart';
+import 'package:flutter_logo_animation/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CustomLoader()
+      home: const Home()
     );
   }
 }
